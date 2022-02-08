b0f19c8cdef792d7eda8dacc98309087133a0ab9
diana97 <liyabereketberta@gmail.com>
2022-02-08 16:41:02 -0500

    adds new rows

--- commands/newgame.js
+++ commands/newgame.js
@@ -47,6 +47,38 @@ function checkBoard(b) {
   return false;
 }
 
+function addRow(b, d, id)
+{
+  b.push(getRand(d));
+    b.push(getRand(d));
+    b.push(getRand(d));
+    images(910,225)
+      .draw(images(`images/${b[(b.length/3-1)*3]}.jpeg`),0,0)
+      .draw(images(`images/${b[(b.length/3-1)*3+1]}.jpeg`),305,0)
+      .draw(images(`images/${b[(b.length/3-1)*3+2]}.jpeg`),610,0)
+      .save(`temp/${id}row${b.length/3}.jpeg`);
+  
+}
+
+const badBoard = [1323,1123,1213,1232,1312,1112,1321,1121,1211,2222,2212,3223];
+
+function newBadBoard(b, d, id) {
+  for (let i = 0; i < 4; i++) {
+    b.push(badBoard[i*3]);
+    b.push(badBoard[i*3+1]);
+    b.push(badBoard[i*3+2]);
+    d.splice(badBoard[i*3]);
+    d.splice(badBoard[i*3+1]);
+    d.splice(badBoard[i*3+2]);
+    images(910,225)
+      .draw(images(`images/${b[i*3]}.jpeg`),0,0)
+      .draw(images(`images/${b[i*3+1]}.jpeg`),305,0)
+      .draw(images(`images/${b[i*3+2]}.jpeg`),610,0)
+      .save(`temp/${id}row${i+1}.jpeg`);
+  }
+  return b;
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('newgame')
@@ -54,8 +86,13 @@ module.exports = {
   async execute(interaction) {
     if (!fs.existsSync('./temp/gameData.json')) {
       let curDeck = [1111,1112,1113,1121,1122,1123,1131,1132,1133,1211,1212,1213,1221,1222,1223,1231,1232,1233,1311,1312,1313,1321,1322,1323,1331,1332,1333,2111,2112,2113,2121,2122,2123,2131,2132,2133,2211,2212,2213,2221,2222,2223,2231,2232,2233,2311,2312,2313,2321,2322,2323,2331,2332,2333,3111,3112,3113,3121,3122,3123,3131,3132,3133,3211,3212,3213,3221,3222,3223,3231,3232,3233,3311,3312,3313,3321,3322,3323,3331,3332,3333];
-      let board = newBoard([], curDeck, interaction.guild.id);
+      let board = newBadBoard([], curDeck, interaction.guild.id);
       let setFound = false;
+
+      while (!checkBoard(board))
+      {
+        addRow(board, curDeck, interaction.guild.id);
+      }
 
       const gameData = {
         "curDeck": curDeck,
@@ -72,10 +109,22 @@ module.exports = {
 
       await interaction.reply('New game!');
 
-      const row1 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row1.jpeg`]});
-      const row2 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row2.jpeg`]});
-      const row3 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row3.jpeg`]});
-      const row4 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row4.jpeg`]});
+      let row1 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row1.jpeg`]});
+      let row2 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row2.jpeg`]});
+      let row3 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row3.jpeg`]});
+      let row4 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row4.jpeg`]});
+      let row5;
+      let row6;
+
+      if (board.length >= 15){
+        row5 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row5.jpeg`]});
+      }
+      if (board.length >= 18)
+      {
+        row6 = await interaction.channel.send({files: [`./temp/${interaction.guild.id}row6.jpeg`]});
+      }
+
+      
 
       try {
         await row1.react('1️⃣');
@@ -90,6 +139,19 @@ module.exports = {
         await row4.react('1️⃣');
         await row4.react('2️⃣');
         await row4.react('3️⃣');
+        if (board.length >= 15)
+        {
+          await row5.react('1️⃣');
+          await row5.react('2️⃣');
+          await row5.react('3️⃣'); 
+        }
+
+        if (board.length >= 18)
+        {
+          await row6.react('1️⃣');
+          await row6.react('2️⃣');
+          await row6.react('3️⃣'); 
+        }
       } catch (error) {
         console.error('One of the emojis failed to react:', error);
       }
