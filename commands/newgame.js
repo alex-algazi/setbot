d9ca65532f500592e3b75c256fa43bcb0789a398
alex-algazi <alyx.algazi@gmail.com>
2022-02-28 21:28:17 -0500

    added bruteforce checking method (checking for sets based on third card)

--- commands/newgame.js
+++ commands/newgame.js
@@ -31,14 +31,62 @@ function newBoard(b, d, id) {
 function checkBoard(b) {
   for (let i = 0; i < b.length; i++) {
     for (let j = i+1; j < b.length; j++) {
-      for (let k = j+1; k < b.length; k++) {
-        if (isSet(b[i],b[j],b[k])) {
-          return true;
-        }
+      if (b.includes(thirdCard(b[i],b[j]))) {
+        return true;
       }
     }
   }
   return false;
+}
+
+function thirdCard(c1, c2) {
+  let total = c1+c2;
+  let result = '';
+  switch(Math.floor((total/1000)%10)%3) {
+    case 0:
+      result += '3';
+      break;
+    case 1:
+      result += '2';
+      break;
+    case 2:
+      result += '1';
+      break;
+  }
+  switch(Math.floor((total/100)%10)%3) {
+    case 0:
+      result += '3';
+      break;
+    case 1:
+      result += '2';
+      break;
+    case 2:
+      result += '1';
+      break;
+  }
+  switch(Math.floor((total/10)%10)%3) {
+    case 0:
+      result += '3';
+      break;
+    case 1:
+      result += '2';
+      break;
+    case 2:
+      result += '1';
+      break;
+  }
+  switch(Math.floor((total/1)%10)%3) {
+    case 0:
+      result += '3';
+      break;
+    case 1:
+      result += '2';
+      break;
+    case 2:
+      result += '1';
+      break;
+  }
+  return parseInt(result);
 }
 
 function addRow(b, d, id) {
@@ -60,10 +108,10 @@ function emojiNum(e) {
 
 function isSet(c1, c2, c3) {
   let total = c1+c2+c3;
-  if (Math.floor((total/1)%10)%3===0 &&
-      Math.floor((total/10)%10)%3===0 &&
+  if (Math.floor((total/1000)%10)%3===0 &&
       Math.floor((total/100)%10)%3===0 &&
-      Math.floor((total/1000)%10)%3===0)
+      Math.floor((total/10)%10)%3===0 &&
+      Math.floor((total/1)%10)%3===0)
     return true;
   else
     return false;
