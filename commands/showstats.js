c328c381c3e799c55804d4cd98824a9e50c2b5e2
alex-algazi <alyx.algazi@gmail.com>
2022-04-15 14:21:35 -0400

    extract input from showstats for game uid, also added a tie in dummy data

--- commands/showstats.js
+++ commands/showstats.js
@@ -4,7 +4,12 @@ const sqlite3 = require('sqlite3').verbose();
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('showstats')
-    .setDescription('Displays server-wide statistics'),
+    .setDescription('Displays server-wide statistics and the results of the last 10 games')
+    .addIntegerOption(option => 
+      option.setName('gameid')
+        .setDescription('Use Game ID for stats about a specific game')
+        .setRequired(false)
+    ),
   async execute(interaction) {
     let db = new sqlite3.Database('database/setbot.db', sqlite3.OPEN_READWRITE, (err) => {
       if (err) {
@@ -19,10 +24,36 @@ module.exports = {
 
     let stats = '';
 
-    // do stuff
+    const gameuid = interaction.options.getInteger('gameid');
+
+    if (gameuid) {
+      stats += gameuid;
+    }
+    else {
+      stats += 'DNE';
+    }
 
     db.close();
 
     await interaction.reply(stats);
   }
 };
+
+/*
+SERVER:
+# of games completed
+% of games cancelled
+average game length
+top 3 players
+last 10 games (gameuid, length, winner/tie)
+
+GAME:
+gameuid
+length
+winner/tie
+users (playername, score)
+
+
+WINNER:
+SELECT a.PlayerUID FROM PlayersGames AS a LEFT OUTER JOIN PlayersGames AS b ON a.GameUID = b.GameUID AND a.Score < b.Score WHERE b.GameUID IS NULL AND a.GameUID = 3;
+*/
--- database/dummydata.sql
+++ database/dummydata.sql
@@ -10,19 +10,19 @@ INSERT INTO ServersPlayers(ServerID, PlayerUID) VALUES (916467574915625020, 3);
 INSERT INTO ServersPlayers(ServerID, PlayerUID) VALUES (916467574915625020, 4);
 INSERT INTO ServersPlayers(ServerID, PlayerUID) VALUES (916467574915625020, 5);
 
-INSERT INTO Games(ServerID, Cancelled, GameStart) VALUES (916467574915625020, 0, DATE('now','-18 hours'));
-INSERT INTO Games(ServerID, Cancelled, GameStart) VALUES (916467574915625020, 1, DATE('now','-3 minutes'));
-INSERT INTO Games(ServerID, Cancelled, GameStart) VALUES (916467574915625020, 0, DATE('now','-4 hours'));
-INSERT INTO Games(ServerID, Cancelled, GameStart) VALUES (916467574915625020, 0, DATE('now','-30 minutes'));
-INSERT INTO Games(ServerID, Cancelled, GameStart) VALUES (916467574915625020, 1, DATE('now','-30 seconds'));
+INSERT INTO Games(ServerID, Cancelled, GameStart, GameEnd) VALUES (916467574915625020, 0, '2022-01-01 00:00:00.000', '2022-01-01 18:00:01.000');
+INSERT INTO Games(ServerID, Cancelled, GameStart, GameEnd) VALUES (916467574915625020, 1, '2022-01-02 00:00:00.000', '2022-01-02 00:03:01.000');
+INSERT INTO Games(ServerID, Cancelled, GameStart, GameEnd) VALUES (916467574915625020, 0, '2022-01-03 00:00:00.000', '2022-01-03 04:00:01.000');
+INSERT INTO Games(ServerID, Cancelled, GameStart, GameEnd) VALUES (916467574915625020, 0, '2022-01-04 00:00:00.000', '2022-01-04 00:30:01.000');
+INSERT INTO Games(ServerID, Cancelled, GameStart, GameEnd) VALUES (916467574915625020, 1, '2022-01-05 00:00:00.000', '2022-01-05 00:00:30.100');
 
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (1, 1, 23);
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (2, 1, 2);
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (1, 2, 5);
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (1, 2, 0);
-INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (1, 3, 4);
+INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (1, 3, 3);
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (2, 3, 5);
-INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (3, 3, 7);
+INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (3, 3, 8);
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (5, 3, 8);
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (1, 4, 9);
 INSERT INTO PlayersGames(PlayerUID, GameUID, Score) VALUES (3, 4, 6);
