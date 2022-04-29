f7bd349e77ddaa507ae46a8ed46657bc118d4939
alex-algazi <alyx.algazi@gmail.com>
2022-04-28 22:06:07 -0400

    added comments to newgame.js

--- commands/newgame.js
+++ commands/newgame.js
@@ -3,6 +3,9 @@ const images = require('images');
 const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 
+/*
+ * Returns a random card code from the cards remaining in the deck
+ */
 function getRand(d) {
   let rand = Math.floor(Math.random()*d.length);
   let card = d[rand];
@@ -10,6 +13,9 @@ function getRand(d) {
   return card;
 }
 
+/*
+ * Generates a new board of 12 cards and generates row images in temp/
+ */
 function newBoard(b, d, id) {
   for (let i = 0; i < 4; i++) {
     b.push(getRand(d));
@@ -24,6 +30,9 @@ function newBoard(b, d, id) {
   return b;
 }
 
+/*
+ * Old check board algorithm, time complexity O(n^3)
+ */
 function checkBoardBruteForce(b) {
   for (let i = 0; i < b.length; i++) {
     for (let j = i+1; j < b.length; j++) {
@@ -37,6 +46,10 @@ function checkBoardBruteForce(b) {
   return false;
 }
 
+/*
+ * New check board algorithm, which utilizes Turán's Theorem
+ * (source: http://pbg.cs.illinois.edu/papers/set.pdf)
+ */
 function checkBoardOptimum(b) {
   let half = Math.ceil(b.length/2);
   let b1 = b.slice(0, half);
@@ -58,10 +71,13 @@ function checkBoardOptimum(b) {
   return false;
 }
 
+/*
+ * Given two cards, finds the card code of the third card which would make a set
+ */
 function thirdCard(c1, c2) {
   let total = c1+c2;
   let result = '';
-  switch(Math.floor((total/1000)%10)%3) {
+  switch(Math.floor((total/1000)%10)%3) { // isolates the 1st digit
     case 0:
       result += '3';
       break;
@@ -72,7 +88,7 @@ function thirdCard(c1, c2) {
       result += '1';
       break;
   }
-  switch(Math.floor((total/100)%10)%3) {
+  switch(Math.floor((total/100)%10)%3) {  // isolates the 2nd digit
     case 0:
       result += '3';
       break;
@@ -83,7 +99,7 @@ function thirdCard(c1, c2) {
       result += '1';
       break;
   }
-  switch(Math.floor((total/10)%10)%3) {
+  switch(Math.floor((total/10)%10)%3) {   // isolates the 3rd digit
     case 0:
       result += '3';
       break;
@@ -94,7 +110,7 @@ function thirdCard(c1, c2) {
       result += '1';
       break;
   }
-  switch(Math.floor((total/1)%10)%3) {
+  switch(Math.floor((total/1)%10)%3) {    // isolates the 4th digit
     case 0:
       result += '3';
       break;
@@ -108,6 +124,9 @@ function thirdCard(c1, c2) {
   return parseInt(result);
 }
 
+/*
+ * Adds three cards to the board from the deck, generates a new row image
+ */
 function addRow(b, d, id) {
   b.push(getRand(d));
   b.push(getRand(d));
@@ -119,23 +138,32 @@ function addRow(b, d, id) {
     .save(`temp/${id}row${b.length/3}.jpeg`);
 }
 
+/*
+ * Simply transforms the emojis 1️⃣ 2️⃣ and 3️⃣ into numbers
+ */
 function emojiNum(e) {
   if (e === '1️⃣') return 1;
   if (e === '2️⃣') return 2;
   if (e === '3️⃣') return 3;
 }
 
+/*
+ * Using modulo arithmetic, checks to see if the three provided cards are a set
+ */
 function isSet(c1, c2, c3) {
   let total = c1+c2+c3;
-  if (Math.floor((total/1000)%10)%3===0 &&
-      Math.floor((total/100)%10)%3===0 &&
-      Math.floor((total/10)%10)%3===0 &&
-      Math.floor((total/1)%10)%3===0)
+  if (Math.floor((total/1000)%10)%3===0 &&  // isolates the 1st digit
+      Math.floor((total/100)%10)%3===0 &&   // isolates the 2nd digit
+      Math.floor((total/10)%10)%3===0 &&    // isolates the 3rd digit
+      Math.floor((total/1)%10)%3===0)       // isolates the 4th digit
     return true;
   else
     return false;
 }
 
+/*
+ * Prints and saves the scores recorded for a given game
+ */
 async function printScores(d, int, can, startTime) {
   if (Object.keys(d).length !== 0) {
     let db = new sqlite3.Database('database/setbot.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -147,14 +175,14 @@ async function printScores(d, int, can, startTime) {
         let ts = new Date();
         console.log(ts.toISOString()+' game in guild '+int.guild.id+' connected to database');
       }
-    });
+    });   // opens the database connction
 
     let convertedTime = startTime.toISOString().slice(0,19).replace('T',' ');
 
     await runPromise(db, `game in guild ${int.guild.id} added to database`, 'INSERT INTO Games(ServerID, Cancelled, GameStart) VALUES (?,?,?)', [int.guild.id, can, convertedTime]);
 
     let sorted = Object.fromEntries(
-      Object.entries(d).sort(([,a],[,b]) => b-a)
+      Object.entries(d).sort(([,a],[,b]) => b-a)    // sorts the scores in descending order
     );
 
     for (let i = 0; i < Object.keys(sorted).length; i++) {
@@ -162,7 +190,7 @@ async function printScores(d, int, can, startTime) {
     }
 
     let longestName = 0;
-    for (const name in sorted) {
+    for (const name in sorted) {        // determines the longest name for table formatting
       if (name.length > longestName) {
         longestName = name.length;
       }
@@ -172,12 +200,15 @@ async function printScores(d, int, can, startTime) {
       scores += `${Object.keys(sorted)[i]}` + ' '.repeat(longestName-Object.keys(sorted)[i].length+1) + '| ' + ' '.repeat(2-sorted[Object.keys(sorted)[i]].toString().length) +  `${sorted[Object.keys(sorted)[i]]}\n`;
     }
     scores += '```';
-    int.channel.send(scores);
+    int.channel.send(scores);     // message sent to channel
 
     db.close();
   }
 }
 
+/*
+ * A promise-based run function for sqlite3
+ */
 function runPromise(db, str, query, params) {
   return new Promise((resolve, reject) => {
     db.run(query, params, (err, rows) => {
@@ -193,12 +224,15 @@ function runPromise(db, str, query, params) {
   });
 }
 
+/*
+ * The core game loop, which contains recirsive calls
+ */
 async function continueGame(board, curDeck, interaction, startTime) {
-  while (!checkBoardOptimum(board)) {
+  while (!checkBoardOptimum(board)) {   // while no set on board, add rows
     addRow(board, curDeck, interaction.guild.id);
   }
 
-  if (fs.existsSync(`temp/${interaction.guild.id}set.jpeg`)) {
+  if (fs.existsSync(`temp/${interaction.guild.id}set.jpeg`)) {    // prints last found set if it exists
     await interaction.channel.send({files: [`temp/${interaction.guild.id}set.jpeg`]});
   }
 
@@ -228,13 +262,27 @@ async function continueGame(board, curDeck, interaction, startTime) {
   let collector4;
   let collector5;
   let collector6;
-  let filter = (reaction, user) => {
+  let filter = (reaction, user) => {    // filter used by all 6 reaction collectors
     return ['1️⃣','2️⃣','3️⃣'].includes(reaction.emoji.name) && user.id != 917630979659685908;
   };
   let playerInputs = {}
   let setFound = false;
   let cancelled = false;
 
+  /*
+   * The following section consists of six reaction collectors, which interpret three
+   * inputs each: one for each card in the row. These can be divided into 4 steps:
+   * 
+   * 1 Send the row image as a message to the channel
+   * 2 react to the message with the following emotes: 1️⃣ 2️⃣ and 3️⃣
+   * 3 create reaction collector
+   * 4 define the 'collect' action for the collector
+   * 
+   * Since the six collectors are mostly identical, notes will only be provided for
+   * the first one. One thing to note is that collectors 2 through 6 are only created
+   * if there are enough cards on the board to require them. So, for instance, if there
+   * are only 9 cards on the board, then only the first 3 collectors are generated.
+   */
   row1 = await interaction.channel.send({files: [`temp/${interaction.guild.id}row1.jpeg`]});
   try {
     await row1.react('1️⃣');
@@ -249,7 +297,7 @@ async function continueGame(board, curDeck, interaction, startTime) {
     reaction.users.remove(user.id);
     let raw = fs.readFileSync(`temp/${interaction.guild.id}data.json`);
     let data = JSON.parse(raw);
-    if (!data.hasOwnProperty(`${user.tag}`)) {
+    if (!data.hasOwnProperty(`${user.tag}`)) {    // creates user score if DNE
       data[`${user.tag}`] = 0;
     }
     let str = JSON.stringify(data);
@@ -274,25 +322,25 @@ async function continueGame(board, curDeck, interaction, startTime) {
         console.log(ts.toISOString()+` user ${user.tag} in guild ${interaction.guild.id} added to "ServersPlayers" table`)
       }
     });
-    if (!setFound && !cancelled) {
+    if (!setFound && !cancelled) { // the rest only works if no one else has found a set or cancelled the game
       if (!playerInputs.hasOwnProperty(`${user.tag}`)) {
         playerInputs[`${user.tag}`] = [];
       }
-      let slot = emojiNum(reaction.emoji.name)-1;
+      let slot = emojiNum(reaction.emoji.name)-1;   // this is the only unique line in the collectors
       let ts = new Date();
       console.log(ts.toISOString()+' '+user.tag+' selected card #'+(slot+1)+' in guild '+interaction.guild.id);
       if (slot !== playerInputs[`${user.tag}`][playerInputs[`${user.tag}`].length-1] && slot !== playerInputs[`${user.tag}`][playerInputs[`${user.tag}`].length-2] && slot !== playerInputs[`${user.tag}`][playerInputs[`${user.tag}`].length-3]) {
         playerInputs[`${user.tag}`].push(slot);
-      }
+      }   // fixes "Jesus's bug": without this check, a player can select the same card three times in a row and get a set
       let select1 = playerInputs[`${user.tag}`][playerInputs[`${user.tag}`].length-1];
       let select2 = playerInputs[`${user.tag}`][playerInputs[`${user.tag}`].length-2];
       let select3 = playerInputs[`${user.tag}`][playerInputs[`${user.tag}`].length-3];
       if (playerInputs[`${user.tag}`].length >= 3) {
-        if (isSet(board[select1],board[select2],board[select3])) {
-          setFound = true;
+        if (isSet(board[select1],board[select2],board[select3])) { // if player finds a set
+          setFound = true;    // lock other players out of finding sets momentarily
           let ts = new Date();
           console.log(ts.toISOString()+' '+user.tag+' found a set in guild '+interaction.guild.id);
-          stopsign.reactions.removeAll();
+          stopsign.reactions.removeAll();   // slooowwwlllyy removes all the emojis from the messages
           row1.reactions.removeAll();
           if (board.length >= 6) {
             row2.reactions.removeAll();
@@ -309,14 +357,14 @@ async function continueGame(board, curDeck, interaction, startTime) {
           if (board.length >= 18) {
             row6.reactions.removeAll();
           }
-          images(910,225)
+          images(910,225)   // generate the image that shows the set found
             .draw(images(`images/borders/${board[select3]}.jpeg`),0,0)
             .draw(images(`images/borders/${board[select2]}.jpeg`),305,0)
             .draw(images(`images/borders/${board[select1]}.jpeg`),610,0)
             .save(`temp/${interaction.guild.id}set.jpeg`);
           let raw = fs.readFileSync(`temp/${interaction.guild.id}data.json`);
           let data = JSON.parse(raw);
-          data[`${user.tag}`] += 1;
+          data[`${user.tag}`] += 1;   // add one point to the user's score
           let str = JSON.stringify(data);
           fs.writeFileSync(`temp/${interaction.guild.id}data.json`, str, err => {
             if (err) {
@@ -325,13 +373,13 @@ async function continueGame(board, curDeck, interaction, startTime) {
             }
           });
           interaction.channel.send(`User ${user.tag} found a set!`);
-          if (curDeck.length !== 0) {
-            if (board.length < 15) {
+          if (curDeck.length !== 0) {   // if there are cards remaining in the deck...
+            if (board.length < 15) {    // replace cards if less than 15 on board
               board[select1] = getRand(curDeck);
               board[select2] = getRand(curDeck);
               board[select3] = getRand(curDeck);
             }
-            else {
+            else {    // bring newer cards into old slots if not
               let cards = playerInputs[`${user.tag}`].slice(-3).sort((a,b)=>{return a-b;}).reverse();
               for (let i = 0; i < 3; i++) {
                 if (cards[i] >= 12) {
@@ -343,7 +391,7 @@ async function continueGame(board, curDeck, interaction, startTime) {
                 }
               }
             }
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 4; i++) {   // regenerate board rows
               images(910,225)
                 .draw(images(`images/${board[i*3]}.jpeg`),0,0)
                 .draw(images(`images/${board[i*3+1]}.jpeg`),305,0)
@@ -351,7 +399,7 @@ async function continueGame(board, curDeck, interaction, startTime) {
                 .save(`temp/${interaction.guild.id}row${i+1}.jpeg`);
             }
           }
-          else {
+          else {    // if no cards in deck
             let cards = playerInputs[`${user.tag}`].slice(-3).sort((a,b)=>{return a-b;}).reverse();
             board.splice(cards[0],1);
             board.splice(cards[1],1);
@@ -364,7 +412,7 @@ async function continueGame(board, curDeck, interaction, startTime) {
                 .save(`temp/${interaction.guild.id}row${i+1}.jpeg`);
             }
           }
-          if (curDeck.length === 0 && !checkBoardOptimum(board)) {
+          if (curDeck.length === 0 && !checkBoardOptimum(board)) {    // if no cards and no sets...
             let raw = fs.readFileSync(`temp/${interaction.guild.id}data.json`);
             let data = JSON.parse(raw);
             if(fs.existsSync(`temp/${interaction.guild.id}data.json`)) {
@@ -373,11 +421,11 @@ async function continueGame(board, curDeck, interaction, startTime) {
             let ts = new Date();
             console.log(ts.toISOString()+' game over in guild '+interaction.guild.id);
             interaction.channel.send('Game over!');
-            printScores(data, interaction, 0, startTime);
+            printScores(data, interaction, 0, startTime);   // end game
           }
-          else {
+          else {    // if there are more sets to be found...
             db.close();
-            continueGame(board, curDeck, interaction, startTime);
+            continueGame(board, curDeck, interaction, startTime);   // continue game
           }
         }
       }
@@ -1139,17 +1187,17 @@ async function continueGame(board, curDeck, interaction, startTime) {
     });
   }
 
-  const gFilter = (reaction, user) => {
+  const gFilter = (reaction, user) => {   // filter used by cancel button
     return reaction.emoji.name === '🛑' && user.id === interaction.user.id;
-  };
+  };    // only user who started the game can canel it
   const gCollector = stopsign.createReactionCollector({filter: gFilter});
   gCollector.on('collect', (reaction, user) => {
-    cancelled = true;
+    cancelled = true;   // set cancelled to true, to avoid synchronicity conflicts
 
     let ts = new Date();
     console.log(ts.toISOString()+' '+interaction.user.tag+' canceled a game in guild '+interaction.guild.id);
 
-    stopsign.reactions.removeAll()
+    stopsign.reactions.removeAll()    // sloooooowly remove all the reactions
     row1.reactions.removeAll()
     if (board.length >= 6) {
       row2.reactions.removeAll()
@@ -1175,36 +1223,38 @@ async function continueGame(board, curDeck, interaction, startTime) {
       fs.unlinkSync(`temp/${interaction.guild.id}data.json`);
     }
     interaction.channel.send('Game canceled.');
-    printScores(data, interaction, 1, startTime);
+    printScores(data, interaction, 1, startTime);   // end game
   });
 }
 
+/*
+ * The entry point for the program
+ */
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('newgame')
     .setDescription('Starts a new game of SET'),
   async execute(interaction) {
-    if (!fs.existsSync(`temp/${interaction.guild.id}data.json`)) {
+    if (!fs.existsSync(`temp/${interaction.guild.id}data.json`)) {    // if game doesn't already exist in this server
       let ts = new Date();
       console.log(ts.toISOString()+' '+interaction.user.tag+' started a game in guild '+interaction.guild.id);
 
-      //let curDeck = [1111,1112,1113,1121,1122,1123,1131,1132,1133,1211,1212,1213,1221,1222,1223,1231,1232,1233,1311,1312,1313,1321,1322,1323,1331,1332,1333];
       let curDeck = [1111,1112,1113,1121,1122,1123,1131,1132,1133,1211,1212,1213,1221,1222,1223,1231,1232,1233,1311,1312,1313,1321,1322,1323,1331,1332,1333,2111,2112,2113,2121,2122,2123,2131,2132,2133,2211,2212,2213,2221,2222,2223,2231,2232,2233,2311,2312,2313,2321,2322,2323,2331,2332,2333,3111,3112,3113,3121,3122,3123,3131,3132,3133,3211,3212,3213,3221,3222,3223,3231,3232,3233,3311,3312,3313,3321,3322,3323,3331,3332,3333];
       let board = newBoard([], curDeck, interaction.guild.id);
 
       await interaction.reply('New game! For help, use the "/howtoplay" command.');
 
-      let gameData = {}
+      let gameData = {}   // initializes the (empty) player scores object
       let str = JSON.stringify(gameData);
       fs.writeFileSync(`temp/${interaction.guild.id}data.json`, str, err => {
         if (err) {
           let ts = new Date();
           console.log(ts.toISOString()+' error writing file', err);
         }
-      });
+      });   // writes the object to the temp folder
 
       let startTime = new Date();
-      continueGame(board, curDeck, interaction, startTime);
+      continueGame(board, curDeck, interaction, startTime);   // first call to game loop
     }
     else {
       await interaction.reply('Game is already active');
